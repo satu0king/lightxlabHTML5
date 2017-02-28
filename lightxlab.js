9fc609c7f95b56472457c08291098e7bc594c943
Satvik Ramaprasad <satviksunkam.ramaprasad@iiitb.org>
2017-03-01 03:15:20 +0530

    added lightRay source, improved UI Buttons
    added light ray source - single light ray
    ui button - transparent buttons, show only when hovering.
    Cleaning up code - removed console logs

--- lightxlab.js
+++ lightxlab.js
@@ -1,4 +1,3 @@
-// // // console.log("working");
 var width = 540;
 var height = 400;
 
@@ -6,14 +5,8 @@ function setup() {
     mirrorList = [];
     radialSourceList = [];
     lightBeamList=[];
-    // lightBeamList.push(new lightBeam(100,100,200,0));
-    // m.push(new mirror(100, 100,  200, 0 * Math.PI / 180));
-    // m.push(new mirror(100, 200,  200, 30 * Math.PI / 180));
-    l = [];
-    for (var i = 0; i < 360; i += 5) {
-        //l.push(new lightRay(0, 0, i * Math.PI / 180));
-    }
-    // rs = new radialSource(100, 100);
+    lightRayList=[];
+
     uiArea.setup();
     simulationArea.setup();
 
@@ -25,8 +18,10 @@ function addMirror() {
 function addLightBeam() {
     lightBeamList.push(new lightBeam(Math.random() * width * .6 + width * .1, Math.random() * height * .3 + height * .6, 150, -Math.PI/2));
 }
+function addLightRay() {
+    lightRayList.push(new lightRaySource(Math.random() * width * .6 + width * .1, Math.random() * height * .3 + height * .6, 150, -Math.PI/2));
+}
 function addRadialSource() {
-    // console.log("ADDED");
     radialSourceList.push(new radialSource(Math.random() * width * .6 + width * .1, Math.random() * height * .8 + height * .1));
 }
 
@@ -37,16 +32,12 @@ var uiArea = {
         this.canvas.width = width;
         this.canvas.height = height;
         this.context = this.canvas.getContext("2d");
-
-        // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        // this.interval = setInterval(update, 50);
         window.addEventListener('mousemove', function(e) {
             var rect = uiArea.canvas.getBoundingClientRect();
             uiArea.mouseX = e.clientX - rect.left;
             uiArea.mouseY = e.clientY - rect.top;
         });
         window.addEventListener('mousedown', function(e) {
-            // // // // console.log(uiArea.mouseX+":"+uiArea.mouseY);
             var rect = uiArea.canvas.getBoundingClientRect();
             uiArea.mouseDownX = e.clientX - rect.left;
             uiArea.mouseDownY = e.clientY - rect.top;
@@ -65,37 +56,33 @@ var uiArea = {
 }
 
 var simulationArea = {
-    canvas: document.getElementById("simulationArea"), //   .createElement("canvas"),
+    canvas: document.getElementById("simulationArea"),
     reset: false,
     setup: function() {
-        this.canvas.width = 560;
-        this.canvas.height = 400;
+        this.canvas.width = width;
+        this.canvas.height = height;
         this.updated = false;
         this.context = this.canvas.getContext("2d");
-
-        // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(update, 20);
 
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        // // // console.log("simulationArea:cleared");
     }
 }
 
 function update() {
-    // uiArea.selected=false;
     if (simulationArea.reset)
         simulationArea.clear();
     uiArea.clear();
-    for (var i = 0; i < l.length; i++)
-        l[i].update();
     simulationArea.sourceChanged = false;
     for (var i = 0; i < radialSourceList.length; i++)
         simulationArea.sourceChanged |= radialSourceList[i].update();
     for (var i = 0; i < lightBeamList.length; i++)
         simulationArea.sourceChanged |= lightBeamList[i].update();
-    // simulationArea.sourceChanged |= rs.update();
+    for (var i = 0; i < lightRayList.length; i++)
+        simulationArea.sourceChanged |= lightRayList[i].update();
+
     simulationArea.reset = false || simulationArea.sourceChanged;
     for (var i = 0; i < mirrorList.length; i++)
         mirrorList[i].update();
@@ -112,7 +99,6 @@ function lightRay(x, y, angle) {
     this.angle = angle;
     this.simulationOver = false;
     this.reset = function() {
-        // // // console.log("reseted");
         this.x = this.originalX;
         this.y = this.originalY;
         this.angle = this.originalAngle;
@@ -127,28 +113,23 @@ function lightRay(x, y, angle) {
         if (this.simulationOver) return;
 
         for (var i = 0; i < 1; i++) {
-            // // console.log(this.x+","+this.y+"::"+180/Math.PI*this.angle);
             var xx = this.x + 100000 * Math.cos(this.angle);
             var yy = this.y + 100000 * Math.sin(this.angle);
             var closestDistance = 100000;
             var newAngle = this.angle;
-            //  // // // console.log(solveMirrorEqn(l1,m1));
             for (var i = 0; i < mirrorList.length; i++) {
                 soln = solveMirrorEqn(this, mirrorList[i]);
 
 
                 if (soln != null) {
-
-                    var dis = distance(this.x, this.y, soln["x"], soln["y"]);
-                    if ((soln["y"] - this.y) * Math.sin(this.angle) > 0 && dis > 0.1 && dis < closestDistance) {
+                    if ( soln["distance"] < closestDistance) {
                         xx = soln["x"];
                         yy = soln["y"];
                         newAngle = soln["angle"];
-                        closestDistance = dis;
+                        closestDistance = soln["distance"];
                     }
                 }
             }
-            // // // // console.log(xx+":"+yy);
             ctx = simulationArea.context;
             ctx.strokeStyle = ("rgba(255,255,0,1)");
             ctx.beginPath();
@@ -156,7 +137,6 @@ function lightRay(x, y, angle) {
             ctx.lineTo(xx, yy);
             ctx.stroke();
             ctx.closePath();
-            // // // // console.log(this.angle*180/Math.PI);
             this.x = xx;
             this.y = yy;
             this.angle = newAngle;
@@ -172,13 +152,10 @@ function radialSource(x, y) {
     this.y = y;
     this.rayList = [];
     this.sourceButton = new Button(x, y, 5);
-
-    // console.log(this.rayList);
     this.reset = function() {
         this.rayList = [];
         for (var i = 0; i < 360; i += 18)
-            this.rayList.push(new lightRay(this.x, this.y, i * Math.PI / 180));
-        // console.log(this.rayList);
+            this.rayList.push(new lightRay(this.x, this.y, i * Math.PI / 180));\
     }
     this.reset();
     this.update = function() {
@@ -209,16 +186,14 @@ function lightBeam(x, y, width, angle) {
     this.height = 5;
     this.angle = angle;
     this.color = "green";
-    this.leftButton = new Button(x, y, 7);
-    this.rightButton = new Button(x + width * Math.cos(angle), y + width * Math.sin(angle), 7);
+    this.leftButton = new Button(x, y, 5,"rgba(0,0,0,0)","rgba(255,255,0,1)");
+    this.rightButton = new Button(x + width * Math.cos(angle), y + width * Math.sin(angle), 5,"rgba(0,0,0,0)","rgba(255,255,0,1)");
 
-
-    // console.log(this.rayList);
     this.reset = function() {
         this.rayList = [];
         for (var i = 0; i < this.width; i += 10)
             this.rayList.push(new lightRay(this.x+i*Math.cos(this.angle), this.y+i*Math.sin(this.angle), this.angle+Math.PI/2));
-        // console.log(this.rayList);
+
     }
     this.reset();
     this.update = function() {
@@ -254,6 +229,43 @@ function lightBeam(x, y, width, angle) {
     }
 }
 
+function lightRaySource(x, y, width, angle) {
+    this.x = x;
+    this.y = y;
+    this.ray;
+    this.width = width;
+    this.height = 5;
+    this.angle = angle;
+    this.leftButton = new Button(x, y, 7,"rgba(200,0,0,1)","rgba(55,0,0,1)");
+    this.rightButton = new Button(x + width * Math.cos(angle), y + width * Math.sin(angle), 7,"rgba(120,0,0,1)","rgba(220,0,0,1)");
+
+    this.reset = function() {
+        this.ray = new lightRay(this.x,this.y,this.angle);
+    }
+    this.reset();
+    this.update = function() {
+        var update=false;
+        update|=this.leftButton.updatePosition();
+        update|=this.rightButton.updatePosition();
+        this.leftButton.update();
+        this.rightButton.update();
+        if (update) {
+            this.x = this.leftButton.x;
+            this.y = this.leftButton.y;
+            this.width=distance(this.leftButton.x,this.leftButton.y,this.rightButton.x,this.rightButton.y);
+            this.angle = Math.atan((this.leftButton.y - this.rightButton.y) / (this.leftButton.x - this.rightButton.x));
+            if (this.rightButton.x <= this.leftButton.x) this.angle += Math.PI;
+            simulationArea.reset = true;
+        }
+          if (simulationArea.reset) {
+             this.reset();
+             //return false;
+        }
+        this.ray.update();
+        return update;
+    }
+}
+
 function solveMirrorEqn(lightRay, mirror) {
     //a1 - lightRay
     //ax+by=c
@@ -270,12 +282,17 @@ function solveMirrorEqn(lightRay, mirror) {
     } else {
         var res_x = ((c1 * b2) - (b1 * c2)) / ((a1 * b2) - (b1 * a2));
         var res_y = ((a1 * c2) - (c1 * a2)) / ((a1 * b2) - (b1 * a2));
+        var dis = distance(lightRay.x, lightRay.y, res_x, res_y);
+
+        if((res_y - lightRay.y) * Math.sin(lightRay.angle) > 0 && dis > 0.1){
         if (((mirror.leftButton.x <= res_x && mirror.rightButton.x >= res_x) || (mirror.leftButton.x >= res_x && mirror.rightButton.x <= res_x)) && ((mirror.leftButton.y <= res_y && mirror.rightButton.y >= res_y) || (mirror.leftButton.y >= res_y && mirror.rightButton.y <= res_y)))
             return {
                 x: res_x,
                 y: res_y,
-                angle: 2 * mirror.angle - lightRay.angle
+                angle: 2 * mirror.angle - lightRay.angle,
+                distance:dis
             };
+        }
         return null;
     }
 
@@ -289,8 +306,8 @@ function mirror(x, y, width, angle) {
     this.height = 5;
     this.angle = angle-.001;
     this.color = "yellow";
-    this.leftButton = new Button(x, y, 7);
-    this.rightButton = new Button(x + width * Math.cos(angle), y + width * Math.sin(angle), 7);
+    this.leftButton = new Button(x, y, 7,"rgba(0,0,0,0)","rgba(255,255,0,1)");
+    this.rightButton = new Button(x + width * Math.cos(angle), y + width * Math.sin(angle), 7,"rgba(0,0,0,0)","rgba(255,255,0,1)");
     this.updated = false;
 
     this.update = function() {
@@ -307,9 +324,7 @@ function mirror(x, y, width, angle) {
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle);
         ctx.fillStyle = this.color;
-        // ctx.strokeStyle = "red";
         ctx.fillRect(0, -this.height / 2, this.width, this.height);
-        // ctx.strokeRect(0, -this.height / 2, this.width, this.height);
         ctx.restore();
         this.leftButton.update();
         this.rightButton.update();
@@ -324,18 +339,26 @@ function mirror(x, y, width, angle) {
 
 }
 
-function Button(x, y, radius) {
+function Button(x, y, radius,color1,color2) {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.color = "red";
+    this.color1 =color1 ;
+    this.color2 =color2 ;
     this.clicked = false;
 
     this.update = function() {
 
       if(this.clicked||(this.isHover()&&!uiArea.selected)){
         var ctx = uiArea.context;
-        ctx.fillStyle = this.color;
+        ctx.fillStyle = this.color2;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.closePath();
+        ctx.fill();
+      }else{
+        var ctx = uiArea.context;
+        ctx.fillStyle = this.color1;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.closePath();
@@ -379,3 +402,4 @@ function distance(x1, y1, x2, y2) {
 document.getElementById("addMirrorButton").addEventListener("click", addMirror);
 document.getElementById("addRadialSourceButton").addEventListener("click", addRadialSource);
 document.getElementById("addLightBeamButton").addEventListener("click", addLightBeam);
+document.getElementById("addLightRayButton").addEventListener("click", addLightRay);
